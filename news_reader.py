5b0862d85d0d83297cf03c23f39d81cd3cdbd604
Yu Lin <gremlinweekend@yus-macbook-air.myfiosgateway.com>
2021-04-08 23:10:10 -0400

    started number 2

--- news_reader.py
+++ news_reader.py
@@ -4,6 +4,15 @@ from sklearn.datasets import fetch_20newsgroups
 class NewsgroupParser:
     ng_data = []
 
+    def fetch_data_20ng(self):
+        ng_train = fetch_20newsgroups(subset='train',
+                                      remove=('headers', 'footers', 'quotes')).data
+        ng_test = fetch_20newsgroups(subset='test',
+                                     remove=('headers', 'footers', 'quotes')).data
+        ng_data = ng_train + ng_test
+        ng_raw = fetch_20newsgroups()
+        return ng_data, ng_raw
+
     def __init__(self):
         newsgroups_train = fetch_20newsgroups(subset='train').data
         newsgroups_test = fetch_20newsgroups(subset='test').data
